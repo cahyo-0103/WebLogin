fbae98e7bf10bdd49bad3c92c11b5964a7159f07
cahyo-0103 <151585229+cahyo-0103@users.noreply.github.com>
2024-10-25 11:47:57 +0700

    jquery2.js

--- jquery2.js
+++ jquery2.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
             // Redirect ke halaman berikutnya setelah login berhasil
             setTimeout(function() {
                 window.location.href = "selamat.html"; // Ganti 'dashboard.html' dengan halaman tujuan
-            }, 2000); // 2 detik delay sebelum pindah halaman
+            }, 500); // 2 detik delay sebelum pindah halaman
         } else {
             $('#loginMessage1').text("Invalid username or password");
         }
